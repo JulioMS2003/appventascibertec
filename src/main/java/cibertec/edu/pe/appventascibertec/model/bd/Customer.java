068a0b56d2baebc6bc3aca92c0d19e436df8595d
JulioMS2003 <julimanchay4862@gmail.com>
2024-04-07 10:08:16 -0500

    primera parte de mapeo de objeto BDD

--- src/main/java/cibertec/edu/pe/appventascibertec/model/bd/Customer.java
+++ src/main/java/cibertec/edu/pe/appventascibertec/model/bd/Customer.java
@@ -1,4 +1,37 @@
 package cibertec.edu.pe.appventascibertec.model.bd;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import org.springframework.data.repository.cdi.Eager;
+
+@Data
+@Entity
+@Table(name = "customers")
 public class Customer {
+    @Id
+    private Integer customerid;
+    @Column(name = "companyname")
+    private String companyname;
+    @Column(name = "contactname")
+    private String contactname;
+    @Column(name = "contacttitle")
+    private String contacttitle;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "city")
+    private String city;
+    @Column(name = "region")
+    private String region;
+    @Column(name = "postalcode")
+    private String postalcode;
+    @Column(name = "country")
+    private String country;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "fax")
+    private String fax;
+
 }
